66896761fcd22eed74173695b003e353e2a4493d
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-02-07 01:54:24 +0200

    vec: Add add() and sub()

--- test.c
+++ test.c
@@ -8,6 +8,7 @@ void test_vec2i(void)
 {
     struct vec2i a = vec2i_ones;
     struct vec2i b = { 1, 2 };
+    struct vec2i c;
 
     ASSERTM_EQ(a.x, 1, "got: %d", a.x);
     ASSERTM_EQ(a.y, 1, "got: %d", a.y);
@@ -19,12 +20,18 @@ void test_vec2i(void)
 
     ASSERT(vec2i_eq((struct vec2i) { -1, -1 }, a));
     ASSERT(!vec2i_eq(a, b));
+
+    ASSERTM(vec2i_eq(c = vec2i_add(b, a), (struct vec2i) {0, 1}),
+            "got %d, %d", c.x, c.y);
+    ASSERTM(vec2i_eq(c = vec2i_sub(b, a), (struct vec2i) {2, 3}),
+            "got %d, %d", c.x, c.y);
 }
 
 void test_vec2f(void)
 {
     struct vec2f a = vec2f_ones;
     struct vec2f b = { 1.0f, 2.0f };
+    struct vec2f c;
 
     ASSERTM_EQ(a.x, 1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, 1.0f, "got: %f", a.y);
@@ -36,12 +43,18 @@ void test_vec2f(void)
 
     ASSERT(vec2f_eq((struct vec2f) { -1.0f, -1.0f }, a));
     ASSERT(!vec2f_eq(a, b));
+
+    ASSERTM(vec2f_eq(c = vec2f_add(b, a), (struct vec2f) {0.0f, 1.0f}),
+            "got %f, %f", c.x, c.y);
+    ASSERTM(vec2f_eq(c = vec2f_sub(b, a), (struct vec2f) {2.0f, 3.0f}),
+            "got %f, %f", c.x, c.y);
 }
 
 void test_vec3f(void)
 {
     struct vec3f a = vec3f_ones;
     struct vec3f b = { 1.0f, 2.0f, 3.0f };
+    struct vec3f c;
 
     ASSERTM_EQ(a.x, 1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, 1.0f, "got: %f", a.y);
@@ -55,12 +68,18 @@ void test_vec3f(void)
 
     ASSERT(vec3f_eq((struct vec3f) { -1.0f, -1.0f, -1.0f }, a));
     ASSERT(!vec3f_eq(a, b));
+
+    ASSERTM(vec3f_eq(c = vec3f_add(b, a), (struct vec3f) {0.0f, 1.0f, 2.0f}),
+            "got %f, %f, %f", c.x, c.y, c.z);
+    ASSERTM(vec3f_eq(c = vec3f_sub(b, a), (struct vec3f) {2.0f, 3.0f, 4.0f}),
+            "got %f, %f, %f", c.x, c.y, c.z);
 }
 
 void test_vec4f(void)
 {
     struct vec4f a = vec4f_ones;
     struct vec4f b = { 1.0f, 2.0f, 3.0f, 4.0f };
+    struct vec4f c;
 
     ASSERTM_EQ(a.x, 1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, 1.0f, "got: %f", a.y);
@@ -76,6 +95,13 @@ void test_vec4f(void)
 
     ASSERT(vec4f_eq((struct vec4f) { -1.0f, -1.0f, -1.0f, -1.0f }, a));
     ASSERT(!vec4f_eq(a, b));
+
+    ASSERTM(vec4f_eq(c = vec4f_add(b, a),
+                     (struct vec4f) {0.0f, 1.0f, 2.0f, 3.0f}),
+            "got %f, %f, %f, %f", c.x, c.y, c.z, c.w);
+    ASSERTM(vec4f_eq(c = vec4f_sub(b, a),
+                     (struct vec4f) {2.0f, 3.0f, 4.0f, 5.0f}),
+            "got %f, %f, %f, %f", c.x, c.y, c.z, c.w);
 }
 
 int main(int argc, char **argv)
--- vec.h
+++ vec.h
@@ -26,6 +26,22 @@ static INLINE bool vec2i_eq(const struct vec2i a, const struct vec2i b)
     return (a.x == b.x) && (a.y == b.y);
 }
 
+static INLINE struct vec2i vec2i_add(const struct vec2i a, const struct vec2i b)
+{
+    return (struct vec2i) {
+        a.x + b.x,
+        a.y + b.y,
+    };
+}
+
+static INLINE struct vec2i vec2i_sub(const struct vec2i a, const struct vec2i b)
+{
+    return (struct vec2i) {
+        a.x - b.x,
+        a.y - b.y,
+    };
+}
+
 
 struct vec2f {
     float x, y;
@@ -41,6 +57,22 @@ static INLINE bool vec2f_eq(const struct vec2f a, const struct vec2f b)
 static INLINE struct vec2f vec2f_inv(const struct vec2f v)
 {
     return (struct vec2f) { -v.x, -v.y };
+}
+
+static INLINE struct vec2f vec2f_add(const struct vec2f a, const struct vec2f b)
+{
+    return (struct vec2f) {
+        a.x + b.x,
+        a.y + b.y,
+    };
+}
+
+static INLINE struct vec2f vec2f_sub(const struct vec2f a, const struct vec2f b)
+{
+    return (struct vec2f) {
+        a.x - b.x,
+        a.y - b.y,
+    };
 }
 
 
@@ -60,6 +92,24 @@ static INLINE struct vec3f vec3f_inv(const struct vec3f v)
     return (struct vec3f) { -v.x, -v.y, -v.z };
 }
 
+static INLINE struct vec3f vec3f_add(const struct vec3f a, const struct vec3f b)
+{
+    return (struct vec3f) {
+        a.x + b.x,
+        a.y + b.y,
+        a.z + b.z,
+    };
+}
+
+static INLINE struct vec3f vec3f_sub(const struct vec3f a, const struct vec3f b)
+{
+    return (struct vec3f) {
+        a.x - b.x,
+        a.y - b.y,
+        a.z - b.z,
+    };
+}
+
 
 struct vec4f {
     float x, y, z, w;
@@ -76,6 +126,27 @@ static INLINE struct vec4f vec4f_inv(const struct vec4f v)
 {
     return (struct vec4f) { -v.x, -v.y, -v.z, -v.w };
 }
+
+static INLINE struct vec4f vec4f_sub(const struct vec4f a, const struct vec4f b)
+{
+    return (struct vec4f) {
+        a.x - b.x,
+        a.y - b.y,
+        a.z - b.z,
+        a.w - b.w,
+    };
+}
+
+static INLINE struct vec4f vec4f_add(const struct vec4f a, const struct vec4f b)
+{
+    return (struct vec4f) {
+        a.x + b.x,
+        a.y + b.y,
+        a.z + b.z,
+        a.w + b.w,
+    };
+}
+
 
 #ifdef __cplusplus
 } /* namespace vm */
