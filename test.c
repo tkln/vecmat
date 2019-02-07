653852ca4b7102f2eab4524fe7201b6b1e2c1281
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-02-07 12:14:19 +0200

    vec: Add eq()

--- test.c
+++ test.c
@@ -7,6 +7,7 @@
 void test_vec2i(void)
 {
     struct vec2i a = vec2i_ones;
+    struct vec2i b = { 1, 2 };
 
     ASSERTM_EQ(a.x, 1, "got: %d", a.x);
     ASSERTM_EQ(a.y, 1, "got: %d", a.y);
@@ -15,11 +16,15 @@ void test_vec2i(void)
 
     ASSERTM_EQ(a.x, -1, "got: %d", a.x);
     ASSERTM_EQ(a.y, -1, "got: %d", a.y);
+
+    ASSERT(vec2i_eq((struct vec2i) { -1, -1 }, a));
+    ASSERT(!vec2i_eq(a, b));
 }
 
 void test_vec2f(void)
 {
     struct vec2f a = vec2f_ones;
+    struct vec2f b = { 1.0f, 2.0f };
 
     ASSERTM_EQ(a.x, 1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, 1.0f, "got: %f", a.y);
@@ -28,11 +33,15 @@ void test_vec2f(void)
 
     ASSERTM_EQ(a.x, -1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, -1.0f, "got: %f", a.y);
+
+    ASSERT(vec2f_eq((struct vec2f) { -1.0f, -1.0f }, a));
+    ASSERT(!vec2f_eq(a, b));
 }
 
 void test_vec3f(void)
 {
     struct vec3f a = vec3f_ones;
+    struct vec3f b = { 1.0f, 2.0f, 3.0f };
 
     ASSERTM_EQ(a.x, 1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, 1.0f, "got: %f", a.y);
@@ -43,11 +52,15 @@ void test_vec3f(void)
     ASSERTM_EQ(a.x, -1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, -1.0f, "got: %f", a.y);
     ASSERTM_EQ(a.z, -1.0f, "got: %f", a.z);
+
+    ASSERT(vec3f_eq((struct vec3f) { -1.0f, -1.0f, -1.0f }, a));
+    ASSERT(!vec3f_eq(a, b));
 }
 
 void test_vec4f(void)
 {
     struct vec4f a = vec4f_ones;
+    struct vec4f b = { 1.0f, 2.0f, 3.0f, 4.0f };
 
     ASSERTM_EQ(a.x, 1.0f, "got: %f", a.x);
     ASSERTM_EQ(a.y, 1.0f, "got: %f", a.y);
@@ -60,6 +73,9 @@ void test_vec4f(void)
     ASSERTM_EQ(a.y, -1.0f, "got: %f", a.y);
     ASSERTM_EQ(a.z, -1.0f, "got: %f", a.z);
     ASSERTM_EQ(a.w, -1.0f, "got: %f", a.w);
+
+    ASSERT(vec4f_eq((struct vec4f) { -1.0f, -1.0f, -1.0f, -1.0f }, a));
+    ASSERT(!vec4f_eq(a, b));
 }
 
 int main(int argc, char **argv)
--- vec.h
+++ vec.h
@@ -1,6 +1,8 @@
 #ifndef VECMAT_VEC_H
 #define VECMAT_VEC_H
 
+#include <stdbool.h>
+
 #include "vecmat-compiler.h"
 
 #ifdef __cplusplus
@@ -19,12 +21,22 @@ static INLINE struct vec2i vec2i_inv(const struct vec2i v)
     return (struct vec2i) { -v.x, -v.y };
 }
 
+static INLINE bool vec2i_eq(const struct vec2i a, const struct vec2i b)
+{
+    return (a.x == b.x) && (a.y == b.y);
+}
+
 
 struct vec2f {
     float x, y;
 };
 
 static const struct vec2f vec2f_ones = { 1.0f, 1.0f };
+
+static INLINE bool vec2f_eq(const struct vec2f a, const struct vec2f b)
+{
+    return (a.x == b.x) && (a.y == b.y);
+}
 
 static INLINE struct vec2f vec2f_inv(const struct vec2f v)
 {
@@ -38,6 +50,11 @@ struct vec3f {
 
 static const struct vec3f vec3f_ones = { 1.0f, 1.0f, 1.0f };
 
+static INLINE bool vec3f_eq(const struct vec3f a, const struct vec3f b)
+{
+    return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+
 static INLINE struct vec3f vec3f_inv(const struct vec3f v)
 {
     return (struct vec3f) { -v.x, -v.y, -v.z };
@@ -49,6 +66,11 @@ struct vec4f {
 };
 
 static const struct vec4f vec4f_ones = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+static INLINE bool vec4f_eq(const struct vec4f a, const struct vec4f b)
+{
+    return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
 
 static INLINE struct vec4f vec4f_inv(const struct vec4f v)
 {
