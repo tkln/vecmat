3703b47344389338a867f673c09ba23c2fb96ce0
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-06-21 12:38:25 +0300

    copmiler: Allow for forcing noinline

--- vecmat-compiler.h
+++ vecmat-compiler.h
@@ -6,13 +6,25 @@
 #ifndef VECMAT_COMPILER_H
 #define VECMAT_COMPILER_H
 
-#if defined(__clang__) || (defined(__GNUC__) && !defined(__INTEL_COMPILER))
-#define VECMAT_INLINE __attribute__((always_inline)) inline
-#elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
-#define VECMAT_INLINE __forceinline
+#if defined(VECMAT_NOINLINE)
+#   if defined(__clang__) || (defined(__GNUC__) && !defined(__INTEL_COMPILER))
+#       define VECMAT_INLINE __attribute__((noinline))
+#   elif defined(_MSC_VER)
+#       define VECMAT_INLINE __declspec(noinline)
+#   elif defined(__INTEL_COMPILER)
+#       warning Unsuported compiler for force noinline
+#   else
+#       warning Unknown compiler, cannot force noinline
+#   endif
 #else
-#warning Unknown compiler, cannot force inline
-#define VECMAT_INLINE inline
+#   if defined(__clang__) || (defined(__GNUC__) && !defined(__INTEL_COMPILER))
+#       define VECMAT_INLINE __attribute__((always_inline)) inline
+#   elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#       define VECMAT_INLINE __forceinline
+#   else
+#       warning Unknown compiler, cannot force inline
+#       define VECMAT_INLINE inline
+#   endif
 #endif
 
 #endif
