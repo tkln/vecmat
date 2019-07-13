059e9d63099456bd5c225fefa2bab952ef864c9d
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-07-13 23:17:57 +0300

    link-test: Don't use using namespace in a header

--- test/link-test-c.h
+++ test/link-test-c.h
@@ -4,14 +4,11 @@
 #include "vec2f.h"
 
 #ifdef __cplusplus
-using vm::vec2f;
 extern "C" {
-#endif
-
-struct vec2f do_stuff(struct vec2f a, struct vec2f b);
-
-#ifdef __cplusplus
+struct vm::vec2f do_stuff(struct vm::vec2f a, struct vm::vec2f b);
 };
+#else
+struct vec2f do_stuff(struct vec2f a, struct vec2f b);
 #endif
 
 #endif
